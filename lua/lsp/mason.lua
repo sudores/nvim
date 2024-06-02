e3ea897cc3c5db715bd17fe0dbac6561f24ec01d
Horobchenko Andrii <andrii.horobchenko>
2024-06-02 17:00:28 +0300

    Added more md and ts/js language servers

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -2,6 +2,6 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "gopls", "clangd", "arduino_language_server", "pylsp", "terraformls" },
+    ensure_installed = { "lua_ls", "gopls", "clangd", "arduino_language_server", "pylsp", "terraformls", "tsserver", "marksman" },
 }
 
