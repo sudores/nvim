7be822bfcbbb68445aa39c9b6d0217a3c7beb4b8
Horobchenko Andrii <andrii.horobchenko>
2024-06-08 19:29:32 +0300

    CM

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -2,6 +2,16 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "gopls", "clangd", "arduino_language_server", "pylsp", "terraformls", "tsserver", "marksman" },
+    ensure_installed = {
+        "lua_ls",
+        "gopls",
+        "clangd",
+        "arduino_language_server",
+        "pylsp",
+        "terraformls",
+        "tsserver",
+        "marksman",
+        "solidity",
+    },
 }
 
