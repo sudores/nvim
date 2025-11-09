d9eeee9bd8722dd5d243f883a7dfe27da0389073
Horobchenko Andrii <andrii.horobchenko>
2025-11-09 21:17:15 +0200

    feat: Added config

--- lua/plugins/vim-speed-dating.lua
+++ lua/plugins/vim-speed-dating.lua
@@ -1,4 +1,18 @@
 return {
     'tpope/vim-speeddating',
-    version = false
+    version = false,
+    config = function ()
+        vim.api.nvim_create_autocmd("VimEnter", {
+          pattern = "*",
+          command = "SpeedDatingFormat %d-%m-%Y",
+        })
+        vim.api.nvim_create_autocmd("VimEnter", {
+          pattern = "*",
+          command = "SpeedDatingFormat %Y-%m",
+        })
+        vim.api.nvim_create_autocmd("VimEnter", {
+          pattern = "*",
+          command = "SpeedDatingFormat %m.%d",
+        })
+    end,
 }
