ce645e8a2386013567636ac40885a2377b958d00
Andrii Horobchenko <andrii@resolv.im>
2025-12-25 12:44:09 +0100

    fix: Added hooks to be loaded

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("keymaps")
 require("options")
+require("hooks")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
